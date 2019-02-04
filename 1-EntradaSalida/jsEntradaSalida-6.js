7d4e11577b952e6cd1900c88ac1ce5b312334312
facu887 <38921447+facu887@users.noreply.github.com>
2019-02-04 12:26:22 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-6.js
+++ 1-EntradaSalida/jsEntradaSalida-6.js
@@ -3,6 +3,12 @@ transformarlos a enteros (parseInt) y Sumarlos.
 Mostar el resulto por medio de "ALERT"*/
 function sumar()
 {
-
+var numero1;
+var numero2;
+var suma;
+numero1 = parseInt (document.getElementById("numeroUno").value);
+numero2 = parseInt (document.getElementById("numeroDos").value);
+suma = numero1 + numero2;
+alert ("La suma es " + suma);
 }
 
--- 1-EntradaSalida/jsEntradaSalida-7.js
+++ 1-EntradaSalida/jsEntradaSalida-7.js
@@ -4,21 +4,37 @@ mostar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
 {	
-	
+var numero1;
+var numero2;
+var suma;
+numero1 = parseInt (document.getElementById("numeroUno").value);
+numero2 = parseInt (document.getElementById("numeroDos").value);
+suma = numero1 + numero2;
+alert ("La suma es " + suma);
 }
 
 function restar()
 {
-	
+var numero1;
+var numero2;
+var resta;
+numero1 = parseInt (document.getElementById("numeroUno").value);
+numero2 = parseInt (document.getElementById("numeroDos").value);
+resta = numero1 - numero2;
+alert ("La resta es " + resta);    
 }
 
 function multiplicar()
 { 
+var numero1;
+var numero2;
 	
 }
 
 function dividir()
 {
+var numero1;
+var numero2;
 	
 }
 
