ee63c21bb5b92bdee0f8c7d056761ebe30ff6b67
facu887 <38921447+facu887@users.noreply.github.com>
2019-02-04 12:29:25 -0300

    Add files via upload

---
+++ jsEntradaSalida-7.js
@@ -0,0 +1,47 @@
+/*Debemos lograr tomar Los numeros por ID ,
+transformarlos a enteros (parseInt),realizar la operación correcta y 
+mostar el resulto por medio de "ALERT"
+ej.: "la Resta es 750"*/ 
+function sumar()
+{	
+var numero1;
+var numero2;
+var suma;
+numero1 = parseInt (document.getElementById("numeroUno").value);
+numero2 = parseInt (document.getElementById("numeroDos").value);
+suma = numero1 + numero2;
+alert ("La suma es " + suma);
+}
+
+function restar()
+{
+var numero1;
+var numero2;
+var resta;
+numero1 = parseInt (document.getElementById("numeroUno").value);
+numero2 = parseInt (document.getElementById("numeroDos").value);
+resta = numero1 - numero2;
+alert ("La resta es " + resta);    
+}
+
+function multiplicar()
+{ 
+var numero1;
+var numero2;
+var multiplicar;
+numero1 = parseInt (document.getElementById("numeroUno").value);
+numero2 = parseInt (document.getElementById("numeroDos").value);
+multiplicar = numero1 * numero2;
+alert ("La multiplicacion es " + multiplicar);	
+}
+
+function dividir()
+{
+var numero1;
+var numero2;
+var dividir;
+numero1 = parseInt (document.getElementById("numeroUno").value);
+numero2 = parseInt (document.getElementById("numeroDos").value);
+	
+}
+
