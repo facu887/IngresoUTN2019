f64e18851c705436932ec1c8ae7f10f79532d729
facu887 <38921447+facu887@users.noreply.github.com>
2019-02-11 12:33:59 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (2).js
+++ 2-InstruccionIf/jsInstruccionIF (2).js
@@ -1,7 +1,10 @@
 function Mostrar()
 {
 //tomo la edad  
-
-
+var edad;
+edad = document.getElementById("edad").value;
+if (edad >= 18) {
+    alert ("Es mayor de edad");
+}
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (4).js
+++ 2-InstruccionIf/jsInstruccionIF (4).js
@@ -1,7 +1,12 @@
 function Mostrar()
 {
 //tomo la edad  
-
-
-
+var edad;
+edad = document.getElementById("edad").value;
+if (edad >= 13) {
+    alert ("Es adolecente");
+}
+else if (edad <= 17) {
+    alert ("Es adolecente");
+    }
 }//FIN DE LA FUNCIÓN
