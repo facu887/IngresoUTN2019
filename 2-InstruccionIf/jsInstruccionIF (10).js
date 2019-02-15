e49b7f9d9a9fefb2761a705924ca6c02828c870b
facu887 <38921447+facu887@users.noreply.github.com>
2019-02-15 12:06:55 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,14 @@
 function Mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
-	
-
+	var nota = Math.floor (Math.random()* 10 + 1);
+	if (nota >= 9 ) {
+		alert ("Excelente, su nota es " + nota);
+	}
+	else if (nota >= 4) {
+		alert ("Aprobó, su nota " + nota);
+	}
+	else {
+		alert ("Vamos, la proxima se puede, su nota es " + nota);
+	}
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (8).js
+++ 2-InstruccionIf/jsInstruccionIF (8).js
@@ -1,8 +1,15 @@
 function Mostrar()
 {
 //tomo la edad  
-
-	
+var edad;
+var estadocivil;
+edad = document.getElementById("edad").value;
+estadocivil = document.getElementById("estadoCivil").value;
+if (edad < 18 && estadocivil  != "Soltero") {
+}
+else if (edad  >= 18 && estadocivil == "Soltero") {
+    alert ("Es soltero y no es menor");
+}
 
 
 }//FIN DE LA FUNCIÓN
