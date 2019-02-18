8f1c107ba7d70b9db58759aaf239e7114c78e871
facu887 <38921447+facu887@users.noreply.github.com>
2019-02-18 12:27:54 -0300

    Add files via upload

--- 3-InstruccionSwitch/jsInstruccionSwitch(3).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(3).js
@@ -4,7 +4,14 @@ function Mostrar()
 var mesDelAño = document.getElementById('mes').value;
 
 //alert (mesDelAño);
-	
+switch (mesDelAño){
+    case "Febrero":
+    alert ("este mes no tiene  mas de 29 dias");
+    break
+    default:
+    alert ("Este mes tiene 30 o mas dias");
+    break;
+}	
 	
 
 
--- 3-InstruccionSwitch/jsInstruccionSwitch(6).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(6).js
@@ -2,7 +2,30 @@ function Mostrar()
 {
 //tomo la hora
 var laHora = document.getElementById('hora').value;
+switch (laHora){
+    case 7:
+    case 8:
+    case 9:
+    case 10:
+    case 11:
 
+    case 12:
+    case 13:
+    case 14:
+    case 15:
+    case 16:
+    case 17:
+    case 18: 
+    case 19:
+
+    case 20:
+    case 21:
+    case 22:
+    case 23:
+    case 24:
+
+    default
+}
 
 
 }//FIN DE LA FUNCIÓN
