0d29eef1c00f014f5a5b46b4b17ed30ebb2ec0bb
facu887 <38921447+facu887@users.noreply.github.com>
2019-03-01 12:23:44 -0300

    Add files via upload

--- 6-InstruccionFor/jsIteracionesFor (6).js
+++ 6-InstruccionFor/jsIteracionesFor (6).js
@@ -1,7 +1,16 @@
 function Mostrar()
 {
+var i;
+var numero;
+var contador;
+contador = 0
+numero = prompt("ingrese un numero");
+for (i=0 ; i <= numero; i++ ) {
+    if (i % 2 == 0) {
+        contador++  ;
+        console.log(i);
+    }
 
-
-
-
+}
+        console.log(contador);
 }//FIN DE LA FUNCIÓN
--- 6-InstruccionFor/jsIteracionesFor (7).js
+++ 6-InstruccionFor/jsIteracionesFor (7).js
@@ -1,7 +1,15 @@
 function Mostrar()
 {
-
-
-
-
+var i;
+var contador; 
+var numero;
+numero = parseInt(prompt ("ingrese un numero"));
+contador = 0;
+for (i=1; i <= numero; i++ ); {
+    if (numero % i == 0) {
+        contador++  ;
+        console.log(i);
+    }
+}
+  console.log(contador);
 }//FIN DE LA FUNCIÓN
