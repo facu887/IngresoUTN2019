cdea4290bf67e0b5b30bfe05af996ff4abefe9d1
facu887 <38921447+facu887@users.noreply.github.com>
2019-03-19 09:52:03 -0300

    Add files via upload

---
+++ s/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char caracter;
+    char palabra (16);
+    printf("Ingrese una palabra: ");
+    scanf("%s", palabra);
+    printf("ud ingreso: %s", palabra);
+    /*int numero;
+    int numero2;
+    int suma;
+    float promedio;
+    printf("Ingrese un numero: ");
+    scanf("%d", &numero);
+    printf("Ingrese un numero: ");
+    scanf("%d", &numero2);
+    suma = numero + numero2;
+    promedio = (float)suma/2;
+    printf("El promedio es %f", promedio);*/
+    return 0;
+}
